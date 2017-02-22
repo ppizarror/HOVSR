dfda85804f89a887c8f3ec17bab4e64a37376983
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-02-22 16:29:00 -0300

    Deleted unused files

--- fourier_transform.m
+++
@@ -1,7 +0,0 @@
-function[freq,mag]=fourier_transform(signal,t,ts,N)
-S=fft(signal,N);
-CS=[S(N/2+1:N) S(1:N/2)];
-freq=[-N/2+1:N/2]/(N*ts);
-mag=abs(CS);
-figure;
-plot(freq,mag);
--- modlim.m
+++
@@ -1,6 +0,0 @@
-function y = modlim(x)
-% Return value of a complex number.
-
-y = sqrt(real(x).^2 + imag(x).^2);
-
-end
